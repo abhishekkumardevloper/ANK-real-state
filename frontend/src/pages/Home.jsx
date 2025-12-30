37f0c5802c8ec913ef52a40f11fd204b36734a20
abhishekkumardevloper <abhishekkumar.devloper@gmail.com>
2025-12-30 22:43:47 +0530

    Fix logo image source path in Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -45,7 +45,7 @@ const Home = () => {
           {/* LOGO IMAGE */}
           <div className="logo">
             <img
-              src="/images/LOgo5.png"
+              src="/public/LOgo5.png"
               alt="ANK Realty"
               className="logo-img"
             />
